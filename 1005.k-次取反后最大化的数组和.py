221d22b44db8aba30019233b1d9a6c720744cb1f
revang <revang.alternative@foxmail.com>
2022-12-11 19:00:37 +0800

    最小堆

--- 1005.k-次取反后最大化的数组和.py
+++ 1005.k-次取反后最大化的数组和.py
@@ -5,10 +5,26 @@
 #
 
 import heapq
-from
+from typing import List
+
 # @lc code=start
 
 
 class Solution:
     def largestSumAfterKNegations(self, nums: List[int], k: int) -> int:
-        # @lc code=end
+        minheap = nums
+        heapq.heapify(minheap)
+
+        for _ in range(k):
+            val = heapq.heappop(minheap)
+            val = -val
+            heapq.heappush(minheap, val)
+
+        return sum(minheap)
+
+
+# @lc code=end
+def test():
+    assert Solution().largestSumAfterKNegations(nums=[4, 2, 3], k=1) == 5
+    assert Solution().largestSumAfterKNegations(nums=[3, -1, 0, 2], k=3) == 6
+    assert Solution().largestSumAfterKNegations(nums=[2, -3, -1, 5, -4], k=2) == 13
