f5f57857cffbecefdc002a04d78386135d70b93e
revang <revang.alternative@foxmail.com>
2022-12-11 19:12:02 +0800

    栈

---
+++ 1021.删除最外层的括号.py
@@ -0,0 +1,32 @@
+#
+# @lc app=leetcode.cn id=1021 lang=python3
+#
+# [1021] 删除最外层的括号
+#
+
+from collections import deque
+
+# @lc code=start
+
+
+class Solution:
+    def removeOuterParentheses(self, s: str) -> str:
+        res = ""
+        stack = deque()
+
+        for c in s:
+            if c == ")":
+                stack.pop()
+            if stack:
+                res += c
+            if c == "(":
+                stack.append(c)
+
+        return res
+
+
+# @lc code=end
+
+def test():
+    assert Solution().removeOuterParentheses(s="(()())(())") == "()()()"
+    # assert Solution().removeOuterParentheses(s="(()())(())") == "()()()"
